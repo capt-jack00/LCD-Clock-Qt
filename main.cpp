6f55588c7a8b2a6b4ac1ae3318baa4c572c3fffc
dbednorz@tuta.io <dbednorz@tuta.io>
2025-01-01 14:42:57 +0100

    Final touches - Added displaying minutes and hours.

--- main.cpp
+++ main.cpp
@@ -5,26 +5,60 @@
 #include <unistd.h>
 #include <QObject>
 #include <QTimer>
+#include <QMainWindow>
+#include <QWidget>
+#include <QHBoxLayout>
 
 using namespace std;
 
 int localSecond;
+int localMinute;
+int localHour;
 
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
-    QLCDNumber clock;
+
+    QMainWindow mainWindow;
+    QWidget centralWidget;
+    QHBoxLayout layout;
+
+    QLCDNumber secondsClock;
+    QLCDNumber minutesClock;
+    QLCDNumber hoursClock;
+
+    layout.addWidget(&hoursClock);
+    layout.addWidget(&minutesClock);
+    layout.addWidget(&secondsClock);
+
+    centralWidget.setLayout(&layout);
+    mainWindow.setCentralWidget(&centralWidget);
+
     QTimer timer;
 
-    QObject::connect(&timer, &QTimer::timeout, [&clock](){
+    QObject::connect(&timer, &QTimer::timeout, [&secondsClock](){
         time_t now = time(nullptr);
         tm* localTime = localtime(&now);
         localSecond = localTime->tm_sec;
-        clock.display(localSecond);
+        secondsClock.display(localSecond);
+    });
+
+    QObject::connect(&timer, &QTimer::timeout, [&minutesClock](){
+        time_t now = time(nullptr);
+        tm* localTime = localtime(&now);
+        localMinute = localTime->tm_min;
+        minutesClock.display(localMinute);
+    });
+
+    QObject::connect(&timer, &QTimer::timeout, [&hoursClock](){
+        time_t now = time(nullptr);
+        tm* localTime = localtime(&now);
+        localHour = localTime->tm_hour;
+        hoursClock.display(localHour);
     });
 
     timer.start(1000);
 
-    clock.show();
+    mainWindow.show();
     return app.exec();
 }
